a9b25e10cee4f9003b4a917eebaa7edf799a93af
smascorro88 <105750582+smascorro88@users.noreply.github.com>
2022-05-17 16:33:40 -0500

    comentario 6

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@
 # comentario de prueba por mascorro hacia mike
 # comentario 4
 # Comentario 5 de mike hacia mascorro
+# comentario 6 de mascorro a mike
 
 from django.contrib import admin
 from django.urls import path
