40defa0b786ff839f4b26743265510cf3cf7539f
Miguel Angel <maagfis@gmail.com>
2022-05-17 16:36:15 -0500

    comentario 7

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,7 @@
 # comentario 4
 # Comentario 5 de mike hacia mascorro
 # comentario 6 de mascorro a mike
+# comentario 7 de mike a mascorr
 
 from django.contrib import admin
 from django.urls import path
